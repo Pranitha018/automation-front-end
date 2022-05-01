5ecef85796d06e43f09f8f9d052cf52d595869cf
Pranitha018 <pranitha018@gmail.com>
2022-05-01 15:39:14 -0700

    Updating Loops.java with loop of prints

--- src/test/java/Loops.java
+++ src/test/java/Loops.java
@@ -1,6 +1,9 @@
 public class Loops {
     public static void main(String [] args) {
         System.out.println("Hello Loops");
+        for(int i=0; i<10; i++){
+            System.out.println("10 Hellos");
+        }
     }
 
 }
